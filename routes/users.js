d7a5575257f961e8707d96a4c565f13394782782
Logan Zoller <s559751@nwmissouri.edu>
2026-03-25 14:10:21 -0500

    add a space

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
   else {
     vOne = vOne - vTwo
   }
-  res.send('Values are: ' + vOne + 'and ' + vTwo);
+  res.send('Values are: ' + vOne + ' and ' + vTwo);
 });
 
 module.exports = router;
